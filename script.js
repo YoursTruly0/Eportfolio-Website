76a7d202f6e7b04951cc53e462da038ba3f87a53
Ahmed Hamdy Mohamed Ahmed Abdelaziz <147906903+YoursTruly0@users.noreply.github.com>
2024-06-21 12:07:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.getElementById('heart').addEventListener('click', function() {
-    this.classList.toggle('red');
+document.querySelectorAll('.heart').forEach(function(heart) {
+    heart.addEventListener('click', function() {
+        this.classList.toggle('red');
+    });
 });
